c19b73770b9da3b686f682e5186af1d0f8ba0758
Tuestag <81398090+Tuestag@users.noreply.github.com>
2021-12-03 20:20:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,27 +32,28 @@ class CocoTrainer(DefaultTrainer):
   
 import base64
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+########################
+main_bg = "Clases.png"
+main_bg_ext = "png"
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
+side_bg = "Clases.png"
+side_bg_ext = "png"
+
+st.markdown(
+    f"""
     <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
     </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
+    """,
+    unsafe_allow_html=True
+)
 
-set_png_as_page_bg("Arcane.S01E07.The.Boy.Savior.12.27.png")
+#######################
 
 @st.cache(persist=True)
 def initialization():
